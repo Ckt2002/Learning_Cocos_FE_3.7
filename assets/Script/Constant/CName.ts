89f7e815c59b80728261a2585c184bb075375dcb
Chung Khanh Toan <toan652002@gmail.com>
2026-05-05 10:02:47 +0700

    Update constant name

---
+++ assets/Script/Constant/CName.ts
@@ -0,0 +1,23 @@
+export const CName = {
+    AnimationName: {
+        AIM: 'aim',
+        DEATH: 'death',
+        HOVERBOARD: 'hoverboard',
+        IDLE: 'idle',
+        IDLE_TURN: 'idle-turn',
+        JUMP: 'jump',
+        PORTAL: 'portal',
+        RUN: 'run',
+        RUN_TO_IDLE: 'run-to-idle',
+        SHOOT: 'shoot',
+        WALK: 'walk',
+    },
+
+    InputName: {
+        MOVE_UP: "MOVE_UP",
+        MOVE_DOWN: "MOVE_DOWN",
+        STOP_MOVING: "STOP_MOVING",
+        SHOOT: "SHOOT",
+        SWITCH_BULLET: "SWITCH_BULLET",
+    }
+} as const
